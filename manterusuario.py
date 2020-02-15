76fad6db50fd540f6bc6ed7909825b8d156d1bc2
João Paulo <jppreti@gmail.com>
2020-02-15 18:36:12 -0400

    Ordenando e exibindo o total de usuários

--- manterusuario.py
+++ manterusuario.py
@@ -1,21 +1,23 @@
-usuarios = []
-opcao = ''
-menu = '''
-[1] - Cadastrar
-[2] - Excluir
-[3] - Exibir todos
+usuarios = [] #lista de usuários para manter em memória principal
+opcao = ''    #guarda a opção escolhida pelo usuário
+#menu que será exibido para o usuário
+menu = '''    
+[1] - Cadastrar novo usuário
+[2] - Excluir um usuário
+[3] - Exibir todos os usuários (total: {})
 
 [0] - Sair
 
 '''
 while opcao != '0':
-    print(menu)
+    print(menu.format(len(usuarios)))
     opcao = input('Digite a opção desejada: ')
 
     if (opcao == '1'): usuarios.append(input('Novo usuário: '))
     if (opcao == '2'): usuarios.remove(input('Usuário a ser excluído: '))
     if (opcao == '3'):
+        usuarios.sort()
         for u in usuarios:
             print(u)
-        input('')
+        input('') #serve apenas para pausar após exibir a lista
 
