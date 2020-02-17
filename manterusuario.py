5e597514ae736f10079c0e3e369e578ddd0dcb2e
João Paulo <jppreti@gmail.com>
2020-02-17 18:19:33 -0400

    Adicionado armazenamento em arquivo para manterusuario.py

--- manterusuario.py
+++ manterusuario.py
@@ -1,3 +1,4 @@
+import os
 usuarios = [] #lista de usuários para manter em memória principal
 opcao = ''    #guarda a opção escolhida pelo usuário
 #menu que será exibido para o usuário
@@ -9,15 +10,24 @@ menu = '''
 [0] - Sair
 
 '''
+with open('usuarios.dat','r') as arquivo:
+    for linha in arquivo:
+        if len(linha.strip())>0: usuarios.append(linha.strip())
+
 while opcao != '0':
-    print(menu.format(len(usuarios)))
+    os.system('clear') or None #limpa a tela do console
+    print(menu.format(len(usuarios))) #adiciona a qtde de usuarios cadastrados no menu
     opcao = input('Digite a opção desejada: ')
 
     if (opcao == '1'): usuarios.append(input('Novo usuário: '))
     if (opcao == '2'): usuarios.remove(input('Usuário a ser excluído: '))
     if (opcao == '3'):
-        usuarios.sort()
+        usuarios.sort() #ordena os elementos da lista
         for u in usuarios:
-            print(u)
+            print(u.strip()) #retira o \n porque o print já faz quebra de linha
         input('') #serve apenas para pausar após exibir a lista
 
+with open('usuarios.dat','w') as arquivo:
+    usuarios.sort()
+    for usuario in usuarios:
+        arquivo.write('\n'+ usuario.strip())
